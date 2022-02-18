21ffbb75fbdc2eab9a33568b96800e80f1c28b7e
femizi <obito.femi@gmail.com>
2022-02-18 15:37:24 +0100

    tweaking

--- components/LongImageCard.tsx
+++ components/LongImageCard.tsx
@@ -2,21 +2,25 @@
 import React from "react";
 import { InView } from "react-intersection-observer";
 
-const LongImageCard = ( {containerRef}) => {
+const LongImageCard = ({ containerRef }) => {
   return (
-    <InView 
-    threshold={1}
-    trackVisibility={true}
-    delay={100}
-    root={containerRef.current}
-    
+    <InView
+      threshold={0.95}
+      trackVisibility={true}
+      delay={100}
+      root={containerRef.current}
     >
       {({ inView, ref, entry }) => (
-        <div ref={ref} className={(entry as IntersectionObserverEntry).intersectionRatio  === 1? "rounded-lg  inline-block transition ease-in-out": "rounded-lg  -mx-2 scale-75 transition ease-in-out inline-block"}>
+        <div
+          ref={ref}
+          className={
+            inView
+              ? "rounded-lg  inline-block transition ease-in-out"
+              : "rounded-lg  -mx-2 scale-75 transition ease-in-out inline-block"
+          }
+        >
           <a href="#">
-            {console.log(entry)}
             <img src="/assets/long.png" alt="image cover" />
-            
           </a>
         </div>
       )}
