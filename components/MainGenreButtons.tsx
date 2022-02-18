d3cfdcb346b053cc1b99c44e00a8124d15b16c7b
femizi <obito.femi@gmail.com>
2022-02-18 12:26:43 +0100

    made genre buttons

--- components/MainGenreButtons.tsx
+++ components/MainGenreButtons.tsx
@@ -1,22 +1,28 @@
-
-
 const MainGenreButtons = () => {
   return (
     <div className="genre-buttons my-6 scrollbar-none overscroll-contain md:hidden flex flex-nowrap gap-4 overflow-y-hidden overflow-x-scroll text-[#576371] font-semibold text-sm ">
-      <div className="rounded-lg bg-[#FF10F0] flex-initial  w-fit px-4 py-2">
-        All
+      <div className=" flex-initial  w-fit ">
+        <button className="rounded-lg bg-[#FF10F0] text-white px-4 py-2 font-semibold ">All</button>
       </div>
-      <div className="rounded-lg bg-slate-300 flex-initial w-fit px-4 py-2">
-        Action
+      <div className=" flex-initial w-fit ">
+        <button className="rounded-lg bg-slate-300 px-4 py-2 font-semibold">
+          Action
+        </button>
       </div>
-      <div className="rounded-lg bg-slate-300 flex-initial w-fit px-4 py-2">
-        Adventure
+      <div className=" flex-initial w-fit ">
+        <button className="rounded-lg bg-slate-300  px-4 py-2 font-semibold">
+          Adventure
+        </button>
       </div>
-      <div className="rounded-lg bg-slate-300 flex-initial w-fit px-4 py-2">
-        Drama
+      <div className=" flex-initial w-fit ">
+        <button className="rounded-lg bg-slate-300  px-4 py-2 font-semibold">
+          Drama
+        </button>
       </div>
-      <div className="rounded-lg bg-slate-300 flex-initial w-fit px-4 py-2">
-        African
+      <div className=" flex-initial w-fit ">
+        <button className="rounded-lg bg-slate-300  px-4 py-2 font-semibold">
+          African
+        </button>
       </div>
     </div>
   );
