0fac0c3ef664ada4ee1796c1e9d95abbf732fb44
femizi <obito.femi@gmail.com>
2022-02-18 15:15:44 +0100

    nothing

--- components/LongImageCard.tsx
+++ components/LongImageCard.tsx
@@ -12,7 +12,7 @@ const LongImageCard = ( {containerRef}) => {
     
     >
       {({ inView, ref, entry }) => (
-        <div ref={ref} className={inView? "rounded-lg scale-75 -mx-4 inline-block transition ease-in-out": "rounded-lg  -mx-10 scale-50 transition ease-in-out inline-block"}>
+        <div ref={ref} className={inView? "rounded-lg scale-95 -mx-4 inline-block transition ease-in-out": "rounded-lg  -mx-10 scale-50 transition ease-in-out inline-block"}>
           <a href="#">
             <img src="/assets/long.png" alt="image cover" />
             
