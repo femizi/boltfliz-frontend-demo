ff63549eab3e7246fc4b35cc00577237cdd96b26
femizi <obito.femi@gmail.com>
2022-02-24 15:28:16 +0100

    likkle changes

--- components/MobileMovieCard.tsx
+++ components/MobileMovieCard.tsx
@@ -5,7 +5,7 @@ const MobileMovieCard = () => {
   return (
     <div className="relative rounded-lg">
       <div className="w-full rounded-lg">
-       <img className="w-full rounded-lg" src="/assets/image cover 6 small.png" alt="image-cover" />
+       <img className="w-full rounded-lg" src="/assets/poster.jpg" alt="image-cover" />
       </div>
       <div className="absolute pr-3 top-0 right-0 pt-3">
       <div className="absolute top-0 right-0    justify-between pr-3 pt-3  font-semibold ">
